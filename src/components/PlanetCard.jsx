f6a97e9455ed54bf271757f3b24875eaf2672995
Arthutepio <arthurodriguesr10@gmail.com>
2022-08-18 18:03:37 -0300

    correção de sintaxe no componente PlanetCard

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,9 +6,8 @@ export default class PlanetCard extends Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">
-          <img src={ planetImage } alt={ `Planeta ${planetName}` } />
-        </p>
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
